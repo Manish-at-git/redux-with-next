ff0bd5bc21cc1f401a6b7125c791b27a4bb9b868
Manish Suthar <mhsuthar@enacton.com>
2022-07-14 15:56:13 +0530

    navbar-links-working

--- pages/[MovieListPage].js
+++ pages/[MovieListPage].js
@@ -37,7 +37,7 @@ function MovieListPage(props) {
                 </small>
               </div>
               <div className={styles.MovieListSort}>
-                <label HTMLfor="sort">Sort by : </label>
+                <label htmlFor={"sort"}>Sort by : </label>
                 <select name="sort" id="sort" form="sortform">
                   <option value="Ranking">Ranking</option>
                   <option value="Release">Release</option>
