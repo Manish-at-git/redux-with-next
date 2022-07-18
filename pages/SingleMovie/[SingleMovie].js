8b6969fb833c47888b21a7ec8670e3013aed84fc
Manish <manishsuthar854@mail.com>
2022-07-18 14:29:10 +0530

    SingleMobvie part one completed

--- pages/SingleMovie/[SingleMovie].js
+++ pages/SingleMovie/[SingleMovie].js
@@ -8,6 +8,8 @@ import { END } from "redux-saga";
 import { getSingleMovie } from "../../redux/actions/main";
 import { useSelector } from "react-redux";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as thinStar } from "@fortawesome/free-regular-svg-icons";
 import {
@@ -29,7 +31,7 @@ function SingleMovie() {
       <Container className={styles.SingleMovie}>
         <div className={styles.SingleMovieHeading}>
           <div className={styles.SingleMovieHeadingTitle}>
-            <h1>{data && data.title}</h1>
+            <h1>{data && data.data.title}</h1>
           </div>
           <div className={styles.SingleMovieHeadingInfo}>
             <div className={styles.Heading}>
@@ -42,7 +44,7 @@ function SingleMovie() {
                 />
                 <span className={styles.Rating}>
                   <span className={styles.BoldLarge}>
-                    {data && data.imDbRating}
+                    {data && data.data.imDbRating}
                   </span>
                   /10
                 </span>
@@ -70,25 +72,27 @@ function SingleMovie() {
                   size="lg"
                   style={{ color: "green" }}
                 />
-                <span className={styles.Rating}>{data && data.imDbRating}</span>
+                <span className={styles.Rating}>
+                  {data && data.data.imDbRating}
+                </span>
               </span>
             </div>
           </div>
         </div>
         <div className={styles.SingleMovieCard}>
           <div className={(styles.CardElement, styles.SingleMoviePoster)}>
-            <img src={data && data.image} />
+            <img src={data && data.data.image} />
           </div>
           <div className={(styles.CardElement, styles.SingleMovieTrailer)}>
-            <div className="embed-responsive embed-responsive-4by3">
+            <div style={{ width: "100%", height: "100%" }}>
               <iframe
-                className="embed-responsive-item"
+                style={{ width: "100%", height: "100%" }}
                 src={data && data.trailer}
               ></iframe>
             </div>
           </div>
-          <div className={(styles.SingleMovieMedia, styles.CardElement)}>
-            <div className={(styles.SingleMovieImages, styles.CardElement)}>
+          <div className={styles.SingleMovieMedia}>
+            <div className={styles.SingleMovieImages}>
               <FontAwesomeIcon
                 className={styles.Icons}
                 icon={faImages}
@@ -108,21 +112,21 @@ function SingleMovie() {
             </div>
           </div>
         </div>
-        <div className={styles.Icons}>
+        <div className={styles.Info}>
           <div className={styles.SingleMovieInfo} style={{ width: "60%" }}>
             <div className={styles.Categories}>
-              {data.title &&
-                data.genreList.map((item) => (
+              {data &&
+                data.data.genreList.map((item) => (
                   <span className={styles.CategoriesAction}>{item.value}</span>
                 ))}
             </div>
-            <div className={styles.Description}>{data && data.plot}</div>
+            <div className={styles.Description}>{data && data.data.plot}</div>
             <hr className={styles.DescriptionLine} />
             <div className={styles.Director}>
               <span className={styles.DirectorBold}>Director</span>
               <span className={styles.DirectorBlue}>
                 {" "}
-                {data && data.directors}
+                {data && data.data.directors}
               </span>
             </div>
             <hr className={styles.DescriptionLine} />
@@ -130,7 +134,7 @@ function SingleMovie() {
               <span className={styles.DirectorBold}>Writer</span>
               <span className={styles.DirectorBlue}>
                 {" "}
-                {data && data.writers}{" "}
+                {data && data.data.writers}{" "}
               </span>
             </div>
             <hr className={styles.DescriptionLine} />
@@ -138,11 +142,11 @@ function SingleMovie() {
               <span className={styles.DirectorBold}>Stars</span>
               <span className={styles.DirectorBlue}>
                 {" "}
-                {data && data.stars}{" "}
+                {data && data.data.stars}{" "}
               </span>
             </div>
           </div>
-          {/* <div
+          <div
             // className="SingleMovie-info
             className={styles.WatchlistBookmark}
           >
@@ -171,23 +175,23 @@ function SingleMovie() {
               </span>
               <span>
                 <b>
-                  <span className="boldLarge">320</span>
+                  <span className={styles.BoldLarge}>320</span>
                 </b>
                 Critic reviews
               </span>
               <span>
                 <b>
                   <span
-                    className="boldLarge"
+                    className={styles.BoldLarge}
                     style={{ backgroundColor: "green", color: "white" }}
                   >
-                    {data && data.metaCriticRating}
+                    {data && data.data.metacriticRating}
                   </span>
                 </b>
                 Metascore
               </span>
             </div>
-          </div> */}
+          </div>
         </div>
       </Container>
     </Container>
